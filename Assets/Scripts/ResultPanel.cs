226dd248784b0a06ac126e7f63abe3880617f5ff
beeeeeeen9677 <107660645+beeeeeeen9677@users.noreply.github.com>
2024-05-23 11:06:01 +0800

    total score

--- Assets/Scripts/ResultPanel.cs
+++ Assets/Scripts/ResultPanel.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 public class ResultPanel : MonoBehaviour
 {
     [SerializeField]
+    private TMPro.TextMeshProUGUI playerNameTxt;
+    [SerializeField]
     private TMPro.TextMeshProUGUI resultTxt;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI totalScoreTxt;
 
-    public void SetResultTxt(int totalScore, float timer)
+    public void SetResultTxt(int score, float timer)
     {
         TimeSpan time = TimeSpan.FromSeconds(timer);
         string timeStr = time.ToString(@"mm\:ss");
         string playerName = PlayerPrefs.GetString("PlayerName");
-        resultTxt.text = "Player: " + playerName + "\nScore: " + totalScore + "\nSurvived: " + timeStr;
+
+        playerNameTxt.text = "Player: " + playerName;
+        resultTxt.text = "Score: " + score + "   Survived: " + timeStr;
+        totalScoreTxt.text = "Total Score: " + ((int)timer + score);
     }
 
 }
